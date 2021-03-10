e9ec589af77e63737f7e8cc681554fbe36e2fdc1
Jacob <jacob.larsen6401@hotmail.dk>
2021-03-11 00:43:08 +0100

    New

---
+++ BookingWeb/BookingWeb/Controllers/BookingController.cs
@@ -0,0 +1,36 @@
+﻿using BookingWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookingWeb.Service;
+
+namespace BookingWeb.Controllers
+{
+    public class BookingController : Controller
+    {
+        [HttpGet]
+        public IActionResult Booking()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Booking(Tour tour)
+        {
+            string message = "";
+
+            if (ModelState.IsValid)
+            {
+                await BookingService.SendMessage(tour);
+                message = tour.Email;
+            }
+            else
+            {
+                message = "Failed to create the product. Please try again";
+            }
+            return Ok();
+        }
+    }
+}
---
+++ BookingWeb/BookingWeb/Service/BookingService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingWeb.Models;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace BookingWeb.Service
+{
+    public class BookingService
+    {
+        public async static Task SendMessage(Tour tour)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs",
+                                        type: "topic");
+
+                var routingKey = tour.BookCancel.ToString();
+                var message = new { tour.Email, tour.Name, tour.BookCancel };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+                channel.BasicPublish(exchange: "topic_logs",
+                                     routingKey: routingKey,
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+            }
+        }
+    }
+}
